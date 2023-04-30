91e79345f0ddcc7e04007ccacf76466ac580c1d1
1TEFYN1 <vladik31032005@gmail.com>
2023-04-30 16:13:34 +0300

    1

---
+++ TaskManager_/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskManager_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskManager taskManager = new TaskManager();
+            ConsoleUI consoleUI = new(taskManager);
+            consoleUI.Run();
+        }
+    }
+}
